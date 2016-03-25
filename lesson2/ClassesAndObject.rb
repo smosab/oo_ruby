5247d8c48221313ae359511e03e78bb44bb45537
Mosab Sasi <smosab@gmail.com>
2016-03-24 21:56:15 -0600

    renamed and moved files around

---
+++ lesson2/ClassesAndObject.rb
@@ -0,0 +1,17 @@
+#1
+
+# class Person
+#   attr_accessor :name
+
+#   def initialize(n)
+#     self.name = n
+#   end
+# end
+
+
+# bob = Person.new('bob')
+# p bob.name                  # => 'bob'
+# bob.name = 'Robert'
+# p bob.name                  # => 'Robert'
+
+#2
