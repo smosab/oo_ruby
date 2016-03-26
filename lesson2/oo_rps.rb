e2bdee0fd44dd67a48c290cf855165076283576a
Mosab Sasi <smosab@gmail.com>
2016-03-26 14:52:14 -0600

    OO Rock Paper Scissors

---
+++ lesson2/oo_rps.rb
@@ -0,0 +1,34 @@
+#OO Rock Paper Scissors
+
+
+class Player
+  def initialize
+    # maybe a "name"? what about a "move"?
+  end
+
+  def choose
+
+  end
+end
+
+class Move
+  def initialize
+    # seems like we need something to keep track
+    # of the choice... a move object can be "paper", "rock" or "scissors"
+  end
+
+  def compare
+
+  end
+end
+
+class Rule
+  def initialize
+    # not sure what the "state" of a rule object should be
+  end
+end
+
+# not sure where "compare" goes yet
+def compare(move1, move2)
+
+end
