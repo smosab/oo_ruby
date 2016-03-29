22936ed513c6ed97382177ffee5199eba017c26d
Mosab Sasi <smosab@gmail.com>
2016-03-28 20:15:10 -0600

    walkthrough OO RPS

--- lesson2/oo_rps.rb
+++ lesson2/oo_rps.rb
@@ -32,3 +32,19 @@ end
 def compare(move1, move2)
 
 end
+
+RPSGame.new.play
+
+class RPSGame
+  def initialize
+
+  end
+
+  def play
+    display_welcom_message
+    human_choose_move
+    computer_choose_move
+    display_winner
+    display_goodbye_message
+  end
+end
