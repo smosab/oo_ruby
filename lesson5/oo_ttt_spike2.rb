212c741c0c5826e37d7c24897794960f6958b4d0
Mosab Sasi <smosab@gmail.com>
2016-04-16 12:18:12 -0600

    oo ttt: added computer moves

--- lesson5/oo_ttt_spike2.rb
+++ lesson5/oo_ttt_spike2.rb
@@ -43,12 +43,14 @@ class Player
 end
 
 class TTTGame
+  HUMAN_MARKER = "X"
+  COMPUTER_MARKER = "O"
   attr_reader :board, :human, :computer
 
   def initialize
     @board = Board.new
-    @human = Player.new("X")
-    @computer = Player.new("O")
+    @human = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
   end
 
   def display_welcome_message
@@ -87,17 +89,23 @@ class TTTGame
     board.set_square_at(square, human.marker)
   end
 
+  def computer_moves
+    board.set_square_at((1..9).to_a.sample, computer.marker)
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
       human_moves
-      display_board
-      break
-      break if someone_won? || board_full?
+
+
+      # break if someone_won? || board_full?
 
       computer_moves
-      break if someone_won? || board_full?
+      display_board
+      # break if someone_won? || board_full?
+      break
     end
     # display_result
     display_goodbye_message
