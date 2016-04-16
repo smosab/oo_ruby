4eb65989e2a494d2b69cd21aeb10f16a4c25edb9
Mosab Sasi <smosab@gmail.com>
2016-04-16 11:55:15 -0600

    oo ttt: setup Board and Square classes

--- lesson5/oo_ttt.rb
+++
@@ -1,9 +0,0 @@
-# 1. Write a description of the problem and extract major nouns and verbs.
-
-Tic tac toe is a game played on a 3 x 3 board. The object of the game is to win by placing three consective 'O's or 'X's on the grid diagnoally or horizontally.
-
-
-
-# 2. Make an initial guess at organizing the verbs into nouns and do a spike to explore the problem with temporary code.
-# 3. Optional - when you have a better idea of the problem, model your thoughts into CRC cards.
-
---
+++ lesson5/oo_ttt_spike2.rb
@@ -0,0 +1,85 @@
+require 'pry'
+
+class Board
+  INITIAL_MARKER = " "
+
+  def initialize
+    @squares = {} #{1 => Square.new(' '), 2 => Square.new(' ')}
+    # @squares[key] = value
+    (1..9).each {|key| @squares[key] = Square.new(INITIAL_MARKER)}
+
+  end
+
+  def get_square_at(key)
+    @squares[key]
+  end
+end
+
+class Square
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
+  end
+end
+
+class Player
+  def initialize
+  end
+
+  def mark
+  end
+end
+
+class TTTGame
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |   #{board.get_square_at(3)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |   #{board.get_square_at(6)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
+    puts "     |     |"
+    puts ""
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      display_board
+      break
+      first_player_moves
+      break if someone_won? || board_full?
+
+      second_player_moves
+      break if someone_won? || board_full?
+    end
+    # display_result
+    display_goodbye_message
+  end
+end
+
+game = TTTGame.new
+game.play
+
